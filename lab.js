9dccacf6b79176de16b38367ade6702a6932de4c
Kendell Andrews <kendellandrews2@gmail.com>
2022-08-18 13:19:35 -0700

    second-upload

---
+++ lab.js
@@ -0,0 +1,153 @@
+/////////// PROBLEM 1 //////////
+
+
+/*
+  Create a variable called 'lovesCode' and set it equal to true. 
+  Check to see if 'lovesCode' is equal to true or false. 
+  If it is true, console log "I love to code!"
+  If it is not, console log "Coding has its challenges."
+*/
+
+
+// //CODE HERE
+// let lovesCode = true
+// if(lovesCode === true){
+//   console.log('I love to code')
+// } else  {
+//   lovesCode === false
+//   console.log('Coding has its challenges.')
+// }
+  
+
+
+
+
+// // For problems 2-3 use the following lines of code:
+// var amysAge = 29;
+// var brittanisAge = 34;
+// var amysBirthYear = 1991;
+// var brittanisBirthYear = 1986;
+
+// ////////// PROBLEM 2 //////////
+
+// /*
+//   Using an if statement and a comparison operator, create code that will check if Amy or Brittani is older, and then console.log '{name} is older'. Hint: Consider what will happen if they are the same age. Handle this situation.
+// */
+
+// //CODE HERE
+
+// var amysAge = 34;
+// var brittanisAge = 34;
+
+// if( amysAge < brittanisAge ){
+//   console.log(`Brittani is older at age ${brittanisAge}`)
+// } else if ( amysAge > brittanisAge ){
+//   console.log(`Amy is older at age ${amysAge}`)
+// } else {
+//   amysAge === brittanisAge 
+//   console.log(`They are the same age at age ${amysAge}`)
+// }
+
+// ////////// PROBLEM 3 //////////
+
+// /*
+//   Using an if statement and a comparison operator, create code that will check if Amy and Brittani were born in the same year. If they were, console.log 'Amy & Brittani were born in the same year'. If they were not, console.log 'Amy & Brittani were not born in the same year'
+// */
+
+// //CODE HERE
+
+// var amysBirthYear = 1989;
+// var brittanisBirthYear = 1986;
+
+
+// if(amysBirthYear === brittanisBirthYear){
+//   console.log('Amy & Brittani were born in the same year')
+// } else {
+//   console.log('Amy & Brittani were not born in the same year')
+// }
+
+
+
+// ////////// PROBLEM 4 //////////
+
+// let temperature = 55;
+// let rain = true;
+
+// /*
+//   Using an if statement and a comparison operator, create code that will log a suggestion on what type of clothes one should wear for the day, based on the above temperature and rain.
+//   If it is 80 degrees or above, and raining, one should wear a t-shirt and take an umbrella. If it is between 60 and 80 degrees, and raining, one should wear a rain-jacket. If it is 60 degrees or below, one should wear a jacket and carry an umbrella. Come up with your own clothing suggestions based on those temperature parameters when it is not raining.
+// */
+
+// //CODE HERE
+
+// let temperature = 85;
+// let rain = false;
+
+// if ( temperature >= 80 && rain === true ){
+//   console.log("One should wear a t-shirt and take an umbrella")
+// } else if (temperature >= 60 && temperature <= 80 && rain === true) {
+//   console.log("One should wear a rain jacket")
+// } else if (temperature <= 60) {
+//   console.log("One should wear a jacket and carry an umbrella")
+// } else {
+//   temperature >= 80
+//   console.log ("Wear shorts and a T-shirt!")
+// }
+
+
+// // ////////// PROBLEM 5 //////////
+
+// // /*
+// //   Create a for-loop that runs 10 times, and on each iteration logs the word 'hello'.
+// // */
+
+// // //CODE HERE
+
+// // for(let i = 0; i < 10; i++){
+// //   console.log('Hello')
+// // }
+
+// // ////////// PROBLEM 6 //////////
+
+// // /*
+// //   Create a for-loop that runs 10 times, logging the numbers 1 through 10. Ex, on the first iteration it would log 1, on the second iteration it would log 2.
+// // */
+
+// // //CODE HERE
+
+// // for ( i = 1; i < 11; i++){
+// //   console.log(i)
+// // }
+
+
+// // ////////// PROBLEM 7 //////////
+
+// // /*
+// //   Create a for-loop that logs the numbers 0 through 10 backwards. Ex, on the first iteration it would log 10, on the second iteration it would log 9.
+// // */
+
+// // //CODE HERE
+
+// for( i = 10; i > 0; i--){
+//   console.log(i)
+// }
+
+
+// // ////////// PROBLEM 8 //////////
+
+// // let score = 0;
+// // let passingScore = 7;
+
+// // /*
+// //   Create a while-loop that logs "Your score is not high enough" while the score variable above is below passingScore. In each iteration of the loop, increase the score by one. If your code is successful, you will see your logged statement 7 times.
+// // */
+
+// // //CODE HERE
+
+// let score = 0;
+// let passingScore = 7;
+
+// while (score < 7){
+//   console.log("Your score is not high enough")
+//   score += 1
+// }
